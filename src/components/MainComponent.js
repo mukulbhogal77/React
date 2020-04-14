9a8077c40849923c3b51c4b00fe2a68191502892
Mukul Bhogal <mukkulbhogal@gmail.com>
2020-04-14 15:21:22 +0530

    React Redux Forms Revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,44 +6,65 @@ import About from './AboutComponent';
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
+const mapDispatchToProps = (dispatch) => ({
+  
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)) ,
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
+
+});
+
+ 
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
-
-     this.state = {
-       dishes: DISHES,
-       comments: COMMENTS,
-       promotions: PROMOTIONS,
-       leaders: LEADERS 
-     };
+ 
   }
   
   
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+  
   render(){
     
-      const HomePage = () => {
-        return(
-            <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-             promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-             leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-             />
-        );
-      }
+    const HomePage = () => {
+      return(
+          <Home 
+              dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+              dishesLoading={this.props.dishes.isLoading}
+              dishesErrMess={this.props.dishes.errMess}
+              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
+    }
 
-      const DishWithId = ({match}) => {
-        return(
-            <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
-        );
-      };
+    const DishWithId = ({match}) => {
+      return(
+          <Dishdetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+            isLoading={this.props.dishes.isLoading}
+            errMess={this.props.dishes.errMess}
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            addComment={this.props.addComment}
+          />
+      );
+    };
 
     
       return (
@@ -51,10 +72,10 @@ class Main extends Component {
         <Header />
           <Switch>
                 <Route path='/home' component={HomePage} />
-                <Route exact path='/about' component={() => <About leaders={this.state.leaders} /> } />
-                <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} /> 
+                <Route exact path='/about' component={() => <About leaders={this.props.leaders} /> } />
+                <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} /> 
                 <Route path='/menu/:dishId' component={DishWithId} />   
-                <Route exact path='/contactus' component={Contact} />
+                <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
                 <Redirect to="/home" />
           </Switch>
         <Footer />
@@ -62,5 +83,5 @@ class Main extends Component {
     );
     }
   }
-
-export default Main;
+ 
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
